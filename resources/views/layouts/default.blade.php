a03df901c9ddc908db784d408ca041897d7041cb
Shao-ye <183610562@qq.com>
2017-02-01 02:05:27 +0800

    init

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title','Sample') - Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      @include('shared.messages')
+      @yield('content')
+      @include('layouts._footer')
+    </div>
+  </body>
+</html>
