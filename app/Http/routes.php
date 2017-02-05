396081c0783785de412e0794ff945f420cbfe6a3
Shao-ye <183610562@qq.com>
2017-02-05 15:15:21 +0800

    Finish user-crud

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,8 +18,18 @@ get('/','StaticPagesController@home')->name('home');
 get('/help','StaticPagesController@help')->name('help');
 get('/about','StaticPagesController@about')->name('about');
 
+
 get('/signup','UsersController@create')->name('signup');
-resource('users','UsersController');
+//resource('users','UsersController');
+get('/users','UsersController@index')->name('users.index');
+get('/users/create','UsersController@create')->name('users.create');
+post('/users','UsersController@store')->name('users.store');
+get('/users/{id}','UsersController@show')->name('users.show');
+get('/users/{id}/edit','UsersController@edit')->name('users.edit');
+patch('/users/{id}','UsersController@update')->name('users.update');
+delete('/users/{id}','UsersController@destroy')->name('users.destroy');
+
+
 
 get('login','SessionsController@create')->name('login');
 post('login','SessionsController@store')->name('login');
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = $users = factory(User::class)->times(50)->make();
+        User::insert($users->toArray());
+
+        $user = User::find(1);
+        $user->name = 'Shaoye';
+        $user->email = '183610562@qq.com';
+        $user->password = 'password';
+        $user->is_admin = true;
+        $user->save();
+    }
+}
