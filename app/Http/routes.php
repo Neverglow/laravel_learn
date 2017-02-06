e2d7007e86230a805a5eaa30fbbe6ab3dfc3e722
Shao-ye <183610562@qq.com>
2017-02-06 20:38:17 +0800

    Finish password reset

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,3 +36,8 @@ get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_ema
 get('login','SessionsController@create')->name('login');
 post('login','SessionsController@store')->name('login');
 delete('logout', 'SessionsController@destroy')->name('logout');
+
+get('password/email','Auth\PasswordController@getEmail')->name('password.reset');
+post('password/email','Auth\PasswordController@postEmail')->name('password.resrt');
+get('password/reset/{token}', 'Auth\PasswordController@getReset')->name('password.edit');
+post('password/reset', 'Auth\PasswordController@postReset')->name('password.update');
