219c9e6694057a76332d15c8c177ca31839154a6
Shao-ye <183610562@qq.com>
2017-02-10 01:15:42 +0800

    Finish following users

--- database/migrations/2017_02_09_134046_create_followers_table.php
+++ database/migrations/2017_02_09_134046_create_followers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatusesTable extends Migration
+class CreateFollowersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,10 @@ class CreateStatusesTable extends Migration
      */
     public function up()
     {
-        Schema::create('statuses', function (Blueprint $table) {
+        Schema::create('followers', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('content');
             $table->integer('user_id')->index();
-            $table->index(['created_at']);
+            $table->integer('follower_id')->index();
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateStatusesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('statuses');  
+        Schema::drop('followers');
     }
 }
